a0bacc761927df75aced2b81f701eb3c0cbd54ec
뎁온누리 <devonnuri@gmail.com>
2018-04-05 20:29:56 +0900

    Disable ESLint 'max-len' and Put traling comma

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,15 @@ module.exports = {
         "ecmaVersion": 6,
         "sourceType": "module",
         "ecmaFeatures": {
-            "jsx": true
-        }
+            "jsx": true,
+        },
     },
     "rules": {
         "require-jsdoc": 0,
         "no-unused-vars": 1,
         "object-curly-spacing": 0,
         "semi": [2, "never"],
-        "linebreak-style": 0
+        "linebreak-style": 0,
+        "max-len": 0,
     }
 };
