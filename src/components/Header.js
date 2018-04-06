86a7f59711983b04f9574699e71737bd70d8f9d3
뎁온누리 <devonnuri@gmail.com>
2018-04-06 23:36:32 +0900

    Header finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
+
 import User from 'react-icons/lib/fa/user'
+import File from 'react-icons/lib/go/file-text'
+import Chart from 'react-icons/lib/fa/line-chart'
+import CaretDown from 'react-icons/lib/fa/caret-down'
 
 import config from '../ctfstrap.config'
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <ul className="header-nav">
-                    <li><a href="#" id="logo">{config.ctfName}</a></li>
-                    <li><a href="#">Challenge</a></li>
-                    <li><a href="#">Rank</a></li>
-                    <li><a href="#" className="header-right"><User size={16} />Account</a></li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    let iconSize = 16
+
+    return (
+      <div className="header">
+        <ul className="header-nav">
+          <li><a href="#" id="logo">{config.ctfName}</a></li>
+          <li><a href="#"><File size={iconSize+2} /> Challenge</a></li>
+          <li><a href="#"><Chart size={iconSize} /> Rank</a></li>
+          <li>
+            <a href="#" className="header-right">
+            <User size={iconSize} />
+            Account
+            <CaretDown size={iconSize} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Header
