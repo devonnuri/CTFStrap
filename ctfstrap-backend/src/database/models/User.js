bad4f7efd5b798623fe601086f9dde2228334294
뎁온누리 <devonnuri@gmail.com>
2018-05-21 21:43:32 +0900

    Login Done!!

--- ctfstrap-backend/src/database/models/User.js
+++ ctfstrap-backend/src/database/models/User.js
@@ -5,51 +5,51 @@ import crypto from 'crypto';
 import { generateToken } from '../../lib/Token';
 import { hash, validate } from '../../lib/Hash';
 
-const UserSchema: Schema = new Schema({
+const User: Schema = new Schema({
   username: String,
   email: String,
   password: String,
   createdAt: Date
 });
 
-const Model = mongoose.model('User', UserSchema);
+class UserClass {
+  static findUser(type: 'email' | 'username', value: string): User {
+    return this.findOne({ [type]: value }).exec();
+  }
 
-Model.findUser = (type: 'email' | 'username', value: string): User => {
-  return Model.findOne({ [type]: value }).exec();
-};
+  static register(username: string, email: string, password: string): User {
+    return new this({
+      username,
+      email,
+      password: hash(password),
+      createdAt: new Date()
+    }).save();
+  }
 
-Model.register = (username: string, email: string, password: string): User => {
-  return new Model({
-    username,
-    email,
-    password: hash(password),
-    createdAt: new Date()
-  }).save();
-};
+  static isExists(email: string, username: string): any {
+    return this.findOne({
+      $or: [{ email }, { username }]
+    }).exec();
+  }
 
-Model.isExists = (email: string, username: string) => {
-  return Model.findOne({
-    $or: [{ email }, { username }]
-  }).exec();
-};
+  validatePassword(password: string): boolean {
+    return validate(password, this.password);
+  }
 
-Model.prototype.validatePassword = (password: string): string => {
-  console.log(Model);
-  return validate(password, Model.password);
-};
+  generateToken(): string {
+    const { _id, username } = this;
 
-Model.prototype.generateToken = (): string => {
-  const { _id, username } = Model;
+    return generateToken(
+      {
+        user: {
+          _id,
+          username
+        }
+      },
+      'user'
+    );
+  }
+}
 
-  return generateToken(
-    {
-      user: {
-        _id,
-        username
-      }
-    },
-    'user'
-  );
-};
-
-export default Model;
+User.loadClass(UserClass);
+export default mongoose.model('User', User);
