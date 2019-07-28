a7255a3900e990b14a0409840583659aef209c9c
devonnuri <devonnuri@gmail.com>
2019-07-28 22:08:25 +0900

    Navbar finish

--- ctfstrap-frontend/src/components/Header.tsx
+++ ctfstrap-frontend/src/components/Header.tsx
@@ -1,37 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    padding: 0.5rem 1rem;
+`;
+
+const Navbar = styled.ul`
+    display: flex;
+    padding: 0;
+    margin: 0;
+    flex-grow: 1;
+`;
+
 const Title = styled.h1`
     display: inline-block;
     vertical-align: middle;
     margin: 1rem 3rem 1rem 0;
 `;
 
-const Navbar = styled.ul`
-    display: inline-block;
-    vertical-align: middle;
-    padding: 0;
-    margin: 0;
-    li {
-        display: inline;
-        padding: 0 1rem;
-    }
+const NavbarLeft = styled.div`
+    display: flex;
+    margin-right: auto;
+`;
+
+const NavbarRight = styled.div`
+    display: flex;
+    margin-left: auto;
+`;
+
+const NavbarItem = styled.div`
+    display: block;
+
+    padding: 0 1rem;
 `;
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
     return (
-        <header>
+        <HeaderContainer>
             <Title>CTFStrap</Title>
             <Navbar>
-                <li>Challenges</li>
-                <li>Scoreboard</li>
-                <li>Login</li>
-                <li>Register</li>
-                <li>Logout</li>
+                <NavbarLeft>
+                    <NavbarItem>Challenges</NavbarItem>
+                    <NavbarItem>Scoreboard</NavbarItem>    
+                </NavbarLeft>
+                <NavbarRight>
+                    <NavbarItem>Login</NavbarItem>
+                    <NavbarItem>Register</NavbarItem>
+                    <NavbarItem>Logout</NavbarItem>
+                </NavbarRight>
             </Navbar>
-        </header>
+        </HeaderContainer>
     );
 };
 
