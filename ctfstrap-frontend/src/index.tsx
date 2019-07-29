e6f4fdbf5c0fb074c3a2576b06d5225dc3f31a2a
devonnuri <devonnuri@gmail.com>
2019-07-29 20:05:39 +0900

    Add redux, react-redux into our stack

--- ctfstrap-frontend/src/index.tsx
+++ ctfstrap-frontend/src/index.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
