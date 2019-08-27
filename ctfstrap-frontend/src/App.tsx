769897a3fbb50c063260416048daf33b597bbf32
devonnuri <devonnuri@gmail.com>
2019-08-27 22:58:44 +0900

    Make sticky footer

--- ctfstrap-frontend/src/App.tsx
+++ ctfstrap-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/base/Header';
 import Core from './components/base/Core';
@@ -8,19 +9,27 @@ import ScoreboardPage from './pages/ScoreboardPage';
 import LoginPage from './pages/LoginPage';
 import LogoutPage from './pages/LogoutPage';
 import RegisterPage from './pages/RegisterPage';
+import Footer from './components/base/Footer';
+
+const Content = styled.div`
+  flex: 1;
+`;
 
 const App: React.FC<{}> = () => {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-        <Route path="/challenges" component={ChallListPage} />
-        <Route path="/scoreboard" component={ScoreboardPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/logout" component={LogoutPage} />
-        <Route path="/register" component={RegisterPage} />
-      </Switch>
+      <Content>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/challenges" component={ChallListPage} />
+          <Route path="/scoreboard" component={ScoreboardPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/logout" component={LogoutPage} />
+          <Route path="/register" component={RegisterPage} />
+        </Switch>
+      </Content>
+      <Footer />
       <Core />
     </Router>
   );
---
+++ ctfstrap-frontend/src/components/base/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Container from './Container';
+
+const FooterContainer = styled.div`
+  text-align: center;
+  padding: 1rem;
+`;
+
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = () => {
+  return (
+    <Container>
+      <FooterContainer>Powered by CTFStrap</FooterContainer>
+    </Container>
+  );
+};
+
+export default Footer;
