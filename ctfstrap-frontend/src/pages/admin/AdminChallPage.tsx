c99238c17fa803ae9343772e821ed24189c383f5
Huiseong Yang <devonnuri@gmail.com>
2019-09-06 22:46:49 +0900

    Fetch challenge list in AdminChallPage

--- ctfstrap-frontend/src/pages/admin/AdminChallPage.tsx
+++ ctfstrap-frontend/src/pages/admin/AdminChallPage.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getChallList } from '../../lib/api/chall';
+import { ChallengeModal } from '../../modules/chall';
 import AdminBasePage from './AdminBasePage';
 
 interface AdminChallPageProps {}
 
 const AdminChallPage: React.FC<AdminChallPageProps> = () => {
+  const [challList, setChallList] = useState<ChallengeModal[]>([]);
+
+  useEffect(() => {
+    getChallList().then(({ data }) => {
+      setChallList(data);
+    });
+  }, []);
+
   return (
     <AdminBasePage>
       <h1>Manage Challenges</h1>
